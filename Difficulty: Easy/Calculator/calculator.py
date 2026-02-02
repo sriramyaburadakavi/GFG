43b220320a3140c1e648a68d162418189d4d885d
sriramyaburadakavi <sriramyaburadakavi@gmail.com>
2026-02-02 21:59:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Calculator/calculator.py
@@ -0,0 +1,11 @@
+def utility(a, b, opr):
+    if(opr == 1):
+        print(str(a + b), end="")
+    elif(opr == 2):
+        print(str(a - b), end="")
+    elif(opr == 3):
+        print(str(a * b), end="")
+    else:
+        print("Invalid Input", end="")
+ 
+
