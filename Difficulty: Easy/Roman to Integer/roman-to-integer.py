451ac3523c7b12a9b24dd05b694211b3274fc404
sriramyaburadakavi <sriramyaburadakavi@gmail.com>
2026-03-16 21:47:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Roman to Integer/roman-to-integer.py
@@ -0,0 +1,35 @@
+class Solution:
+
+    def romanToDecimal(self, s): 
+
+        roman_map = {
+
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+
+            'C': 100, 'D': 500, 'M': 1000
+
+        }
+
+        
+
+        total = 0
+
+        n = len(s)
+
+        
+
+        for i in range(n):
+
+            # If current value is less than next value, subtract
+
+            if i < n - 1 and roman_map[s[i]] < roman_map[s[i+1]]:
+
+                total -= roman_map[s[i]]
+
+            else:
+
+                total += roman_map[s[i]]
+
+        
+
+        return total
